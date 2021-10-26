9ed65e1a799bab9736c1dfcf70be92ab4766e1d9
Dustin Flynn <flynnd297@gmail.com>
2021-10-26 14:08:54 -0500

    Adjusted the ApplicationDbContext.cs to use DbSet and modelBuilder.

--- eCommerceStarterCode/Models/Product.cs
+++ eCommerceStarterCode/Models/Product.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace eCommerceStarterCode.Models
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; }
         public string ProductName { get; set; }
         public string ProductDescription { get; set; }
-        public string Price { get; set; }
+        public int Price { get; set; }
         public string Genre { get; set; }
     }
 }
--- eCommerceStarterCode/Models/ShoppingCart.cs
+++ eCommerceStarterCode/Models/ShoppingCart.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace eCommerceStarterCode.Models
 {
     public class ShoppingCart
     {
+        [Key]
         public int UserId { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }
