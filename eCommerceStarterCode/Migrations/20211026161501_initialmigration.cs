5001bbca78bc9e2ef61fe5d56368c85e3ecdc545
joeblk74 <joeblk@gmail.com>
2021-10-26 08:35:44 -0800

    Troubleshoot migrations

---
+++ eCommerceStarterCode/Migrations/20211026161501_initialmigration.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eCommerceStarterCode.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a85197de-2346-492e-861e-08b0370b485f");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c073f42c-79e8-41a6-a5d7-0ed41ae7aca0");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "527df8b3-23f8-40d5-9b6a-e1eb4e2b611a", "4d6dbb54-4298-4ceb-a323-d25f75d44756", "User", "USER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "f9cb2613-9f5a-4416-a464-c407c590af77", "576ef950-e6d1-4e28-a90e-30e94a638d29", "Admin", "ADMIN" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "527df8b3-23f8-40d5-9b6a-e1eb4e2b611a");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f9cb2613-9f5a-4416-a464-c407c590af77");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "c073f42c-79e8-41a6-a5d7-0ed41ae7aca0", "df8961da-91f7-4729-a4aa-312b6fcd7c8f", "User", "USER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "a85197de-2346-492e-861e-08b0370b485f", "27e54b6b-7578-4229-8a3e-7a5a5651df3b", "Admin", "ADMIN" });
+        }
+    }
+}
--- eCommerceStarterCode/Models/UserRole.cs
+++ eCommerceStarterCode/Models/UserRole.cs
@@ -10,7 +10,6 @@ namespace eCommerceStarterCode.Models
     {
         public int UserId { get; set; }
         public int RoleId { get; set; }
-        public virtual Role Role { get; set; }
         public virtual User User { get; set; }
     }
 }
