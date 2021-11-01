f8adf2e85eccd8de7aa0ba92fa69e68685acca45
Dustin Flynn <flynnd297@gmail.com>
2021-11-01 09:50:47 -0500

    Built usercontroller in order to be able to pull user information on the front end.

---
+++ eCommerceStarterCode/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/users/")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        [HttpGet("user")]
+        public IActionResult GetAllUsers()
+        {
+            var users = _context.Users;
+            return Ok(users);
+        }
+
+        
+        [HttpGet("{userId}"), Authorize]
+        public IActionResult GetCurrentUser(string userId)
+        {
+            var user = _context.Users.Where(u => u.Id == userId).SingleOrDefault();
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+        
+    }
+}
